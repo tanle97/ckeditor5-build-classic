5188e8c4dc9335a8e15018461124d19b8621652a
Le Ngoc Tan <tanlengoc091997@gmail.com>
2019-11-14 16:20:46 +0700

    v15.2.0

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -39,7 +39,27 @@ import Font from '@ckeditor/ckeditor5-font/src/font';
 import Highlight from '@ckeditor/ckeditor5-highlight/src/highlight';
 import HorizontalLine from '@ckeditor/ckeditor5-horizontal-line/src/horizontalline';
 
-export default class ClassicEditor extends ClassicEditorBase {}
+const colors = {
+	columns: 9,
+	documentColors: 18,
+	colors: [
+		{ color: 'hsl(0, 0%, 0%)', label: 'hsl(0, 0%, 0%)' }, { color: 'hsl(0, 0%, 12.5%)', label: 'hsl(0, 0%, 12.5%)' }, { color: 'hsl(0, 0%, 25%)', label: 'hsl(0, 0%, 25%)' }, { color: 'hsl(0, 0%, 37.5%)', label: 'hsl(0, 0%, 37.5%)' }, { color: 'hsl(0, 0%, 50%)', label: 'hsl(0, 0%, 50%)' }, { color: 'hsl(0, 0%, 62.5%)', label: 'hsl(0, 0%, 62.5%)' }, { color: 'hsl(0, 0%, 75%)', label: 'hsl(0, 0%, 75%)' }, { color: 'hsl(0, 0%, 87.5%)', label: 'hsl(0, 0%, 87.5%)' }, { color: 'hsl(0, 0%, 100%)', label: 'hsl(0, 0%, 100%)' },
+		{ color: 'hsl(0, 100%, 10%)', label: 'hsl(0, 100%, 10%)' }, { color: 'hsl(0, 100%, 20%)', label: 'hsl(0, 100%, 20%)' }, { color: 'hsl(0, 100%, 30%)', label: 'hsl(0, 100%, 30%)' }, { color: 'hsl(0, 100%, 40%)', label: 'hsl(0, 100%, 40%)' }, { color: 'hsl(0, 100%, 50%)', label: 'hsl(0, 100%, 50%)' }, { color: 'hsl(0, 100%, 60%)', label: ' ' }, { color: 'hsl(0, 100%, 70%)', label: 'hsl(0, 100%, 70%)' }, { color: 'hsl(0, 100%, 80%)', label: 'hsl(0, 100%, 80%)' }, { color: 'hsl(0, 100%, 90%)', label: 'hsl(0, 100%, 90%)' },
+		{ color: 'hsl(30, 100%, 10%)', label: 'hsl(30, 100%, 10%)' }, { color: 'hsl(30, 100%, 20%)', label: 'hsl(30, 100%, 20%)' }, { color: 'hsl(30, 100%, 30%)', label: 'hsl(30, 100%, 30%)' }, { color: 'hsl(30, 100%, 40%)', label: 'hsl(30, 100%, 40%)' }, { color: 'hsl(30, 100%, 50%)', label: 'hsl(30, 100%, 50%)' }, { color: 'hsl(30, 100%, 60%)', label: 'hsl(30, 100%, 60%)' }, { color: 'hsl(30, 100%, 70%)', label: 'hsl(30, 100%, 70%)' }, { color: 'hsl(30, 100%, 80%)', label: 'hsl(30, 100%, 80%)' }, { color: 'hsl(30, 100%, 90%)', label: 'hsl(30, 100%, 90%)' },
+		{ color: 'hsl(60, 100%, 10%)', label: 'hsl(60, 100%, 10%)' }, { color: 'hsl(60, 100%, 20%)', label: 'hsl(60, 100%, 20%)' }, { color: 'hsl(60, 100%, 30%)', label: 'hsl(60, 100%, 30%)' }, { color: 'hsl(60, 100%, 40%)', label: 'hsl(60, 100%, 40%)' }, { color: 'hsl(60, 100%, 50%)', label: 'hsl(60, 100%, 50%)' }, { color: 'hsl(60, 100%, 60%)', label: 'hsl(60, 100%, 60%)' }, { color: 'hsl(60, 100%, 70%)', label: 'hsl(60, 100%, 70%)' }, { color: 'hsl(60, 100%, 80%)', label: 'hsl(60, 100%, 80%)' }, { color: 'hsl(60, 100%, 90%)', label: 'hsl(60, 100%, 90%)' },
+		{ color: 'hsl(90, 100%, 10%)', label: 'hsl(90, 100%, 10%)' }, { color: 'hsl(90, 100%, 20%)', label: 'hsl(90, 100%, 20%)' }, { color: 'hsl(90, 100%, 30%)', label: 'hsl(90, 100%, 30%)' }, { color: 'hsl(90, 100%, 40%)', label: 'hsl(90, 100%, 40%)' }, { color: 'hsl(90, 100%, 50%)', label: 'hsl(90, 100%, 50%)' }, { color: 'hsl(90, 100%, 60%)', label: 'hsl(90, 100%, 60%)' }, { color: 'hsl(90, 100%, 70%)', label: 'hsl(90, 100%, 70%)' }, { color: 'hsl(90, 100%, 80%)', label: 'hsl(90, 100%, 80%)' }, { color: 'hsl(90, 100%, 90%)', label: 'hsl(90, 100%, 90%)' },
+		{ color: 'hsl(120, 100%, 10%)', label: 'hsl(120, 100%, 10%)' }, { color: 'hsl(120, 100%, 20%)', label: 'hsl(120, 100%, 20%)' }, { color: 'hsl(120, 100%, 30%)', label: 'hsl(120, 100%, 30%)' }, { color: 'hsl(120, 100%, 40%)', label: 'hsl(120, 100%, 40%)' }, { color: 'hsl(120, 100%, 50%)', label: 'hsl(120, 100%, 50%)' }, { color: 'hsl(120, 100%, 60%)', label: 'hsl(120, 100%, 60%)' }, { color: 'hsl(120, 100%, 70%)', label: 'hsl(120, 100%, 70%)' }, { color: 'hsl(120, 100%, 80%)', label: 'hsl(120, 100%, 80%)' }, { color: 'hsl(120, 100%, 90%)', label: 'hsl(120, 100%, 90%)' },
+		{ color: 'hsl(150, 100%, 10%)', label: 'hsl(150, 100%, 10%)' }, { color: 'hsl(150, 100%, 20%)', label: 'hsl(150, 100%, 20%)' }, { color: 'hsl(150, 100%, 30%)', label: 'hsl(150, 100%, 30%)' }, { color: 'hsl(150, 100%, 40%)', label: 'hsl(150, 100%, 40%)' }, { color: 'hsl(150, 100%, 50%)', label: 'hsl(150, 100%, 50%)' }, { color: 'hsl(150, 100%, 60%)', label: 'hsl(150, 100%, 60%)' }, { color: 'hsl(150, 100%, 70%)', label: 'hsl(150, 100%, 70%)' }, { color: 'hsl(150, 100%, 80%)', label: 'hsl(150, 100%, 80%)' }, { color: 'hsl(150, 100%, 90%)', label: 'hsl(150, 100%, 90%)' },
+		{ color: 'hsl(180, 100%, 10%)', label: 'hsl(180, 100%, 10%)' }, { color: 'hsl(180, 100%, 20%)', label: 'hsl(180, 100%, 20%)' }, { color: 'hsl(180, 100%, 30%)', label: 'hsl(180, 100%, 30%)' }, { color: 'hsl(180, 100%, 40%)', label: 'hsl(180, 100%, 40%)' }, { color: 'hsl(180, 100%, 50%)', label: 'hsl(180, 100%, 50%)' }, { color: 'hsl(180, 100%, 60%)', label: 'hsl(180, 100%, 60%)' }, { color: 'hsl(180, 100%, 70%)', label: 'hsl(180, 100%, 70%)' }, { color: 'hsl(180, 100%, 80%)', label: 'hsl(180, 100%, 80%)' }, { color: 'hsl(180, 100%, 90%)', label: 'hsl(180, 100%, 90%)' },
+		{ color: 'hsl(210, 100%, 10%)', label: 'hsl(210, 100%, 10%)' }, { color: 'hsl(210, 100%, 20%)', label: 'hsl(210, 100%, 20%)' }, { color: 'hsl(210, 100%, 30%)', label: 'hsl(210, 100%, 30%)' }, { color: 'hsl(210, 100%, 40%)', label: 'hsl(210, 100%, 40%)' }, { color: 'hsl(210, 100%, 50%)', label: 'hsl(210, 100%, 50%)' }, { color: 'hsl(210, 100%, 60%)', label: 'hsl(210, 100%, 60%)' }, { color: 'hsl(210, 100%, 70%)', label: 'hsl(210, 100%, 70%)' }, { color: 'hsl(210, 100%, 80%)', label: 'hsl(210, 100%, 80%)' }, { color: 'hsl(210, 100%, 90%)', label: 'hsl(210, 100%, 90%)' },
+		{ color: 'hsl(240, 100%, 10%)', label: 'hsl(240, 100%, 10%)' }, { color: 'hsl(240, 100%, 20%)', label: 'hsl(240, 100%, 20%)' }, { color: 'hsl(240, 100%, 30%)', label: 'hsl(240, 100%, 30%)' }, { color: 'hsl(240, 100%, 40%)', label: 'hsl(240, 100%, 40%)' }, { color: 'hsl(240, 100%, 50%)', label: 'hsl(240, 100%, 50%)' }, { color: 'hsl(240, 100%, 60%)', label: 'hsl(240, 100%, 60%)' }, { color: 'hsl(240, 100%, 70%)', label: 'hsl(240, 100%, 70%)' }, { color: 'hsl(240, 100%, 80%)', label: 'hsl(240, 100%, 80%)' }, { color: 'hsl(240, 100%, 90%)', label: 'hsl(240, 100%, 90%)' },
+		{ color: 'hsl(270, 100%, 10%)', label: 'hsl(270, 100%, 10%)' }, { color: 'hsl(270, 100%, 20%)', label: 'hsl(270, 100%, 20%)' }, { color: 'hsl(270, 100%, 30%)', label: 'hsl(270, 100%, 30%)' }, { color: 'hsl(270, 100%, 40%)', label: 'hsl(270, 100%, 40%)' }, { color: 'hsl(270, 100%, 50%)', label: 'hsl(270, 100%, 50%)' }, { color: 'hsl(270, 100%, 60%)', label: 'hsl(270, 100%, 60%)' }, { color: 'hsl(270, 100%, 70%)', label: 'hsl(270, 100%, 70%)' }, { color: 'hsl(270, 100%, 80%)', label: 'hsl(270, 100%, 80%)' }, { color: 'hsl(270, 100%, 90%)', label: 'hsl(270, 100%, 90%)' },
+		{ color: 'hsl(300, 100%, 10%)', label: 'hsl(300, 100%, 10%)' }, { color: 'hsl(300, 100%, 20%)', label: 'hsl(300, 100%, 20%)' }, { color: 'hsl(300, 100%, 30%)', label: 'hsl(300, 100%, 30%)' }, { color: 'hsl(300, 100%, 40%)', label: 'hsl(300, 100%, 40%)' }, { color: 'hsl(300, 100%, 50%)', label: 'hsl(300, 100%, 50%)' }, { color: 'hsl(300, 100%, 60%)', label: 'hsl(300, 100%, 60%)' }, { color: 'hsl(300, 100%, 70%)', label: 'hsl(300, 100%, 70%)' }, { color: 'hsl(300, 100%, 80%)', label: 'hsl(300, 100%, 80%)' }, { color: 'hsl(300, 100%, 90%)', label: 'hsl(300, 100%, 90%)' },
+		{ color: 'hsl(330, 100%, 10%)', label: 'hsl(330, 100%, 10%)' }, { color: 'hsl(330, 100%, 20%)', label: 'hsl(330, 100%, 20%)' }, { color: 'hsl(330, 100%, 30%)', label: 'hsl(330, 100%, 30%)' }, { color: 'hsl(330, 100%, 40%)', label: 'hsl(330, 100%, 40%)' }, { color: 'hsl(330, 100%, 50%)', label: 'hsl(330, 100%, 50%)' }, { color: 'hsl(330, 100%, 60%)', label: 'hsl(330, 100%, 60%)' }, { color: 'hsl(330, 100%, 70%)', label: 'hsl(330, 100%, 70%)' }, { color: 'hsl(330, 100%, 80%)', label: 'hsl(330, 100%, 80%)' }, { color: 'hsl(330, 100%, 90%)', label: 'hsl(330, 100%, 90%)' }
+	]
+};
+
+export default class ClassicEditor extends ClassicEditorBase { }
 
 // Plugins to include in the build.
 ClassicEditor.builtinPlugins = [
@@ -117,7 +137,36 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	fontSize: {
-		options: [ 'default', 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40 ]
+		options: ['default', 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
+	},
+	fontColor: colors,
+	fontBackgroundColor: colors,
+	fontFamily: {
+		options: [
+			'default',
+			'Arial, Helvetica, sans-serif',
+			'Roboto, Helvetica, Arial, sans-serif',
+			'Helvetica, Roboto, Arial, sans-serif',
+			'Courier New, Courier, monospace',
+			'Georgia, serif',
+			'Lucida Sans Unicode, Lucida Grande, sans-serif',
+			'Tahoma, Geneva, sans-serif',
+			'Times New Roman, Times, serif',
+			'Trebuchet MS, Helvetica, sans-serif',
+			'Verdana, Geneva, sans-serif',
+			'Ubuntu, Arial, sans-serif',
+			'Ubuntu Mono, Courier New, Courier, monospace'
+		]
+	},
+	highlight: {
+		options: [
+			{ model: 'yellowMarker', class: 'marker-yellow', title: 'Yellow Marker', color: 'var(--ck-highlight-marker-yellow)', type: 'marker' },
+			{ model: 'greenMarker', class: 'marker-green', title: 'Green marker', color: 'var(--ck-highlight-marker-green)', type: 'marker' },
+			{ model: 'pinkMarker', class: 'marker-pink', title: 'Pink marker', color: 'var(--ck-highlight-marker-pink)', type: 'marker' },
+			{ model: 'blueMarker', class: 'marker-blue', title: 'Blue marker', color: 'var(--ck-highlight-marker-blue)', type: 'marker' },
+			{ model: 'redPen', class: 'pen-red', title: 'Red pen', color: 'var(--ck-highlight-pen-red)', type: 'pen' },
+			{ model: 'greenPen', class: 'pen-green', title: 'Green pen', color: 'var(--ck-highlight-pen-green)', type: 'pen' }
+		]
 	},
 	image: {
 		toolbar: [
